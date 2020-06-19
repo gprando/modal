08d73685aa3f5bfb60295eb4a3836b9c4d9b46aa
Gabriel Prando <gprando55@gmail.com>
2020-06-19 12:23:20 -0300

    add modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Modal from './components/Modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setIsOpen(true)} >Open</button>
+      {isOpen ? <Modal onClose={() => setIsOpen(false)}>Modal</Modal> : null}
     </div>
   );
 }
---
+++ src/components/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  background-color: rgba(0,0,0,0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background-color: #fff;  
+    color: #000;
+    width: 60%;
+    height: 60%;
+    border-radius: 20px;
+  
+    .close {
+      background-color: transparent;
+      outline: none;
+      width: 32px;
+      height: 32px;
+      right: calc(-100% + 64px);
+      top: 16px;
+      cursor: pointer;
+      position: relative;
+      display: flex;
+      align-items: center;
+
+      &:before, &:after {
+        content: '';
+        position: absolute;
+        width: 2.5px;
+        height: 24px; 
+        background-color: #000;
+      }
+      
+      &:before {
+        transform: rotate(45deg);
+      }
+      &:after {
+        transform: rotate(-45deg);
+      }
+    }
+  }
+
+`;
